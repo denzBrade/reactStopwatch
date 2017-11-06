125018d926fe9028cfeb5d196ab8dc75fa47e58e
Denzil Brade <denzbrade@gmail.com>
2017-11-06 13:05:11 +0000

    remove unused css file & update button styles

--- stopwatch-app/src/App.js
+++ stopwatch-app/src/App.js
@@ -63,7 +63,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React Js Stopwatch App</h1>
+          <h1 className="App-title">React JS Stopwatch App</h1>
         </header>
 
         <TimeElapsed id="timer" timeElapsed={timeElapsed} />
